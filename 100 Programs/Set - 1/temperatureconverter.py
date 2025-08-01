f4a43b94bfcf3605c37934ed8ec6a05a86d1f8d2
Bandi Venkat Chowdary <bandivenkatchowdary.study@gmail.com>
2025-08-01 10:24:33 +0530

    Temperature Converter Program Added

---
+++ 100 Programs/Set - 1/temperatureconverter.py
@@ -0,0 +1,5 @@
+#Program to convert temperature from Celsius to Fahrenheit
+celsius = int(input("Enter Temperature(In Celsius): "))
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} Celsius = {fahrenheit} Fahrenheit")
